3e42ce8b0eeb04a4b36e26d26c145a23fca417d0
yugn <38592271+yugn@users.noreply.github.com>
2022-01-08 03:10:24 +0500

    New mode added: move unique files from all sources into the given destination.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 
 setup(
     name="yadupe",
-    version="1.0.2",
+    version="1.1.0",
     description="Recursively scan one or more given directories for duplicate files.",
     long_description=README,
     long_description_content_type="text/markdown",
